480333f7452a08643e94d315532726690118111c
ExtranoDev <jeomade19@gmail.com>
2023-01-27 14:01:35 +0000

    Made code cleaner by removing name repitition

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,8 @@
 export default function getBudgetObject(...args) {
   const budget = {
-    income: args[0],
-    gdp: args[1],
-    capita: args[2],
+    income,
+    gdp,
+    capita,
   };
 
   return budget;
