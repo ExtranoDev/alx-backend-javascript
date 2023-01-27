0d839878806b2be8e2a51ce2c225ef3a72d2edc1
ExtranoDev <jeomade19@gmail.com>
2023-01-27 21:21:22 +0000

    Returns types of classroom

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+/**
+ * Implements a function and it will return array of objects
+ */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
