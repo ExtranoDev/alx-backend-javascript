0709b1c570b2a088100a93fb158ba7d177c1961e
ExtranoDev <jeomade19@gmail.com>
2023-02-01 02:19:24 +0000

    Gets students unique Id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
 export default function getListStudentIds(args) {
   if (Array.isArray(args)) {
-    const ids = [];
-    for (const arg of args) {
-      ids.push(arg.id);
-    }
-    return ids;
+    return args.map(arg => arg.id);
   }
   return [];
 }
