3d1cae139425ffc1743cfdd0d95ff675fa4133dd
ExtranoDev <jeomade19@gmail.com>
2023-02-01 03:04:23 +0000

    WeakMap ES6

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+  count += 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
