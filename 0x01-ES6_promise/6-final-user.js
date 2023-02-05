c5df3b8010bc01de536da95454d61a877c25ad01
ExtranoDev <jeomade19@gmail.com>
2023-02-05 23:12:04 +0000

    Returns an array of struture

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => {
+      const struct = [];
+      response.foreach((resp) => struct.push({
+        status: resp.status, value: resp.value || `${resp.reason}`,
+      }));
+      return struct;
+    });
+}
