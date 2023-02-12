d1230c293c41f89c88ae807ab9e3e9db39718b55
ExtranoDev <jeomade19@gmail.com>
2023-02-12 21:09:16 +0000

    Clean set element

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,12 @@
 // returns a string of all the set values
 // start with a specific string
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string' || !(set instanceof Set)) return '';
   const cleanString = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      cleanString.push(value.split(startString)[1]);
+      cleanString.push(value.slice(startString.length));
     }
   });
-  return cleanString.reduce((aV, cV) => `${aV}-${cV}`);
+  return cleanString.join('-');
 }
