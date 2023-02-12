ae87b6d8a22da0f6125d58dec40d65c9d639a861
ExtranoDev <jeomade19@gmail.com>
2023-02-12 14:43:34 +0000

    Using the filter keyword

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+// Create a function getStudentsByLocation
+// returns an array of objects who are located in a specific city
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}
