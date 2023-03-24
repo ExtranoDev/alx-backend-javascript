92b9876cd00f0bd791fce6b11e66d4723388dc58
ExtranoDev <jeomade19@gmail.com>
2023-03-24 23:34:43 +0000

    Update test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,13 +5,16 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', function () {
-  it('First number rounded', function () {
-    assert.equal(calculateNumber(1.5, 5), calculateNumber(2, 5));
+  it('Confirms output', function () {
+    assert.strictEqual(calculateNumber(1.5, 5), 7);
+    assert.strictEqual(calculateNumber(1, 3.6), 5);
+    assert.strictEqual(calculateNumber(2.3, 5.8), 8);
+    assert.strictEqual(calculateNumber(2, 4.8), 7);
+    assert.strictEqual(calculateNumber(2.3, 9), 11);
+    assert.strictEqual(calculateNumber(2.3, 3.2), 5);
   });
-  it('Second number rounded', function () {
-    assert.equal(calculateNumber(1, 3.6), calculateNumber(1, 4));
-  });
-  it('Both number rounded', function () {
-    assert.equal(calculateNumber(2.3, 5.8), calculateNumber(2, 6));
+  it('Check arguements', function () {
+    assert.strictEqual(isNaN(calculateNumber(0)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
