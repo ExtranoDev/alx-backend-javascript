d3d399525d6a2e3412a3bc6ee1c7e79e34f5cfe2
ExtranoDev <jeomade19@gmail.com>
2023-03-24 17:26:57 +0000

    basic HTTP server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// HTTP server using Node's HTTP module
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
