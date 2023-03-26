748b3708168afd9c6876708462f1864349877ed3
ExtranoDev <jeomade19@gmail.com>
2023-03-26 17:17:10 +0000

    Async tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,9 +2,10 @@ const chai = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function () {
-  it('test async function', function() {
+  it('test async function', function(done) {
     getPaymentTokenFromAPI(true).then((data) => {
-      chai.expect(data).to.equal({data: 'Successful response from the API' });
-    });
+      chai.expect(data).to.include({data: 'Successful response from the API' });
+      done();
+    }).catch((error) => done(error));
   });
 });
