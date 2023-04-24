12eb2051f24f877ce6f29104ebc12d093654b8ac
ExtranoDev <jeomade19@gmail.com>
2023-04-24 08:14:39 +0000

    Added more interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -11,6 +11,7 @@ const student1: Student = {
   age: 21,
   location: "Ilara"
 };
+
 const student2: Student = {
   firstName: "Jude",
   lastName: "Nwaneri",
@@ -19,3 +20,28 @@ const student2: Student = {
 };
 
 const studentsList: Student[] = [student1, student2];
+const stud_prop = ['firstName', 'lastName', 'age', 'location']
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+const head_row = document.createElement('tr');
+const head_det = document.createElement('th');
+
+for (const i in stud_prop) {
+  head_det.textContent = i;
+  head_row.appendChild(head_det);
+}
+
+tbody.appendChild(head_row);
+
+studentsList.forEach((obj) => {
+  const row = document.createElement('th');
+  const detail = document.createElement('td');
+  Object.values(obj).forEach((prop) => {
+    detail.textContent = prop;
+    row.appendChild(detail);
+  });
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,56 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+console.log(printTeacher(teacher3.firstName, teacher3.lastName));
